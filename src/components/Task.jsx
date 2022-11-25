9faab26c9b1e83e0277165c2ffd9da3e247112d8
rudzzz <rudsolza13@gmail.com>
2022-11-25 13:06:29 -0300

    code

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Task.css"
 
-const Task = ({task}) => {
+const Task = ({task, handleTaskClick}) => {
     return (
-        <div className="task-container">
-            {task.title}
+        <div className="task-container" style={task.complete ? {borderLeft: '6px solid rgb(187, 192, 42)'} : {}}>
+            
         </div>
+        // <div className="task-container">
+        //     {task.title}
+        // </div>
     )
     
 }
