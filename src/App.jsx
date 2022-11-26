5f9363ba612a377b85c9624cbfe0045199682673
rudzzz <rudsolza13@gmail.com>
2022-11-26 11:46:25 -0300

    code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import {v4 as uuidv4} from "uuid"
+import Header from "./components/Header";
 
 const App = () => {
   // let message = "Hello, World!";
@@ -29,20 +30,31 @@ const App = () => {
     setTasks(newTask);
   }
 
-  const handleTaskClick = (taskiId) => {
+  const handleTaskClick = (taskId) => {
     const newTasks = tasks.map(task => {
-      if(task.id == taskiId) return {... task, completed: !task.completed}
+      if(task.id == taskId) return {... task, completed: !task.completed}
 
       return task;
     });
     setTasks(newTasks);
   }
 
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <div className="container">
+        <Header/>
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+        <Tasks 
+          tasks={tasks} 
+          handleTaskClick={handleTaskClick}
+          handleTaskDeletion={handleTaskDeletion}
+        />
       </div>
     </>
   )
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import "./Task.css"
+import "./Task.css";
+import {CgClose, CgInfo} from "react-icons/cg";
 
-const Task = ({task, handleTaskClick}) => {
+const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
     return (
-        <div className="task-container" style={task.complete ? {borderLeft: '6px solid rgb(187, 192, 42)'} : {}}>
-            
+        <div className="task-container" style={task.completed ? {borderLeft: '6px solid rgb(187, 192, 42)'} : {}}>
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+            <div className="buttons-container">
+                <button className="remove-task" onClick={() => handleTaskDeletion(task.id)}>
+                    <CgClose/>
+                </button>
+                <button className="task-detail">
+                    <CgInfo/>
+                </button>
+            </div>
         </div>
-        // <div className="task-container">
-        //     {task.title}
-        // </div>
     )
     
 }
